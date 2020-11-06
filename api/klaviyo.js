77c5c359b5e8622fbd22bf2c1eacbf950accf513
gr0-autt <anthony.utt@gr0.com>
2020-11-06 10:44:05 -0500

    Klaviyo lambda function setup

---
+++ api/klaviyo.js
@@ -0,0 +1,22 @@
+exports.handler = async function(event, context) {
+    const axios = require('axios');
+    const parser = require('lambda-multipart-parser');
+
+    const clientData = {
+        "rellery": { apiKey: "PsYYrt", listId: "JTZTmW" },
+    };
+    
+    const data = await parser.parse(event);
+
+    const { apiKey, listId } = clientData[data.client];
+
+    const res = await axios.post(`https://a.klaviyo.com/api/v2/list/${listId}/subscribe`, {
+        api_key: apiKey,
+        profiles: [
+            email: data.email
+        ]
+    }, { headers: { 'Content-Type': 'application/json' });
+
+    console.log(res);
+    return { statusCode: res.status, body: JSON.stringify({ message: res.statusText })};
+}
