e820dc695bdc01482dd2866044a8c21d9be877f1
gr0-autt <anthony.utt@gr0.com>
2020-12-15 11:36:31 -0500

    CI/CD pipeline testing

--- .travis/build.py
+++ .travis/build.py
@@ -3,13 +3,20 @@ import os
 def main():
     folder_list = []
     with open('/tmp/updates', 'r') as diff_list:
-        folder_list = [f for f in diff_list.readlines() if '/' in f and f[0] != '.']
+        file_list = [f for f in diff_list.readlines() if '/' in f and f[0] != '.']
 
-    for folder in folder_list:
+    complete = []
+    for file_ in file_list:
+        folder = file_[:file_.find('/')]
+        if folder in complete:
+            continue
+        
         client = folder[:folder.rfind('-')]
         url = folder[:folder.rfind('-')] + '.' + folder[folder.rfind('-') + 1:]
 
         os.system('.travis/deploy.sh %s %s %s' % (folder, client, url))
+
+        complete.append(folder)
 
 if __name__ == '__main__':
     main()
